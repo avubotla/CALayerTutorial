21bf554c5bdaee1cd328d88eacf5d177bc1e9159
VIPadm <vipadm@vipadms-imac.local>
2018-04-25 14:31:00 +0530

    added circle layer for image

--- CALayerTutorial/ViewController.swift
+++ CALayerTutorial/ViewController.swift
@@ -10,15 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var box: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        getCircleImage()
     }
+    
+    func getCircleImage() {
+        box.layer.cornerRadius = 100
+        box.layer.shadowOffset = CGSize(width: 6, height: 6)
+        box.layer.shadowOpacity = 0.7
+        box.layer.shadowRadius = 6
+        box.layer.shadowColor = UIColor(red: 44.0/255.0, green: 62.0/255.0, blue: 80.0/255.0, alpha: 1.0).cgColor
+        box.layer.borderColor = UIColor.black.cgColor
+        box.layer.borderWidth = 3
+        
+        box.layer.contents = UIImage(named: "tree.jpg")?.cgImage
+        box.layer.contentsGravity = kCAGravityResize
+        box.layer.masksToBounds = true
+        
+        box.layer.backgroundColor = UIColor.black.cgColor
+        box.layer.opacity = 0.6
+    }
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
